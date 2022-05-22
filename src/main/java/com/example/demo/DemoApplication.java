0dd5f427f834acdb5af7a0646a983660a8c79ea7
Ivan Bowen <bowenivan16@gmail.com>
2022-05-22 11:58:01 +0300

    adding some explanation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 //import org.apache.catalina.core.ApplicationContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
