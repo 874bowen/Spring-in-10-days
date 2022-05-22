c7f617bd5d4de49396e35ffae9a4f4ecdaee50ff
Ivan Bowen <bowenivan16@gmail.com>
2022-05-22 18:10:47 +0300

    @Qualifier gives some identity as to a bean and will be used when we want to resolve the conflict btwn dependencies

--- src/main/java/com/example/demo/BubbleSortAlgorithm.java
+++ src/main/java/com/example/demo/BubbleSortAlgorithm.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 // When you have two classes with @component you can use @Primary to give more emphasis to a class
 @Component
-@Primary
+//@Primary
+@Qualifier("bubble")
 public class BubbleSortAlgorithm implements SortAlgorithm{
     public int[] sort(int[] numbers){
         // Logic for Bubble Sort
