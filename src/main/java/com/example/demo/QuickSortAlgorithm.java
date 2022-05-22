f11ecd6d00db1d902e77a656f4b40bcd686ff23f
Ivan Bowen <bowenivan16@gmail.com>
2022-05-22 18:12:15 +0300

    autowiring by @Qualifier

--- src/main/java/com/example/demo/QuickSortAlgorithm.java
+++ src/main/java/com/example/demo/QuickSortAlgorithm.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Primary
+// If one of the candidate among the algorithms and the candidate is favorite in all the situations then use @Primary(Most efficient)
+// If at some point we prefer a certain candidate to another then @Primary is not the most suitable way
+// You can then use autowiring by name or by @Qualifier()
+// Another option to be able to resolve multiple candidates available is by using @Qualifier("....")
+@Qualifier("quick")
 public class QuickSortAlgorithm implements SortAlgorithm {
     public int[] sort(int[] numbers){
         // Logic for Bubble Sort
