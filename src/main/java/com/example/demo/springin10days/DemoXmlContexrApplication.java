0e6e42338e594fea88ab0336cf845028b95bd258
Ivan Bowen <bowenivan16@gmail.com>
2022-05-24 10:37:18 +0300

    xmlContext way back time

--- src/main/java/com/example/demo/springin10days/DemoXmlContexrApplication.java
+++ src/main/java/com/example/demo/springin10days/DemoXmlContexrApplication.java
@@ -6,18 +6,15 @@ import com.example.demo.springin10days.xml.XmlPersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan("com.example.demo.componentScan") //without this it'll take the springin10days package only
+//@Configuration
+//@ComponentScan("com.example.demo.componentScan") //without this it'll take the springin10days package only
 public class DemoXmlContexrApplication {
     // What are the beans?
     // What are the dependencies of a bean
     // Where to search for a bean? =>< No need because we are in the same package = Component Scan
     private static Logger LOGGER = LoggerFactory.getLogger(DemoScopeApplication.class);
-    // we were doing logging using System.out.println now let's use the SLF4J Logger above
     public static void main(String[] args) {
         // new QuickSortAlgorithm() --> this is a bean
 //		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
@@ -27,9 +24,11 @@ public class DemoXmlContexrApplication {
         try {
             ClassPathXmlApplicationContext applicationContext =
                     new ClassPathXmlApplicationContext("applicationContext.xml");
+            LOGGER.info("Beans Loaded -> {}", (Object)applicationContext.getBeanDefinitionNames());
+            // [xmlJdbcConnection, xmlPersonDAO]
             XmlPersonDAO xmlPersonDAO = applicationContext.getBean(XmlPersonDAO.class);
             System.out.println(xmlPersonDAO);
-            LOGGER.info("{}", xmlPersonDAO);
+            LOGGER.info("{} {}", xmlPersonDAO, xmlPersonDAO.getXmlJdbcConnection());
         } catch (BeansException e) {
             throw new RuntimeException(e);
         }
