57078fca0530c6e2c3c7518902ca46f4104259aa
bowen <bowenivan16@gmail.com>
2022-05-26 13:24:51 +0300

    not using spring boot tests but Spring we change the annotations

---
+++ src/test/java/com/example/demo/springin10days/basic/BinarySearchImplTests.java
@@ -0,0 +1,27 @@
+package com.example.demo.springin10days.basic;
+
+import com.example.demo.springin10days.DemoApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertEquals;
+
+
+//Load the context
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = DemoApplication.class) // load configuration by class
+public class BinarySearchImplTests {
+    // Get this bean from the context
+    @Autowired
+    BinarySearchImpl binarySearch;
+    @Test
+    public void testBasicScenario(){
+        // call method on binarySearch
+        int result = binarySearch.binarySearch(new int[]{}, 5);
+        // check if the value is correct
+        assertEquals(3, result);
+    }
+}
